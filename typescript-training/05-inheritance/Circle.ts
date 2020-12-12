6325e1e093de5c7671d4ce847fd3d12d87da41fc
kneess <anibalcampos2324@gmail.com>
2020-12-11 19:49:12 -0700

    inheritance-abstractClasses-interfaces

---
+++ typescript-training/05-inheritance/Circle.ts
@@ -0,0 +1,13 @@
+import { Shape } from "./Shape";
+
+export class Circle extends Shape {
+
+    constructor(theX: number, theY: number,
+                private _radius: number){
+                    super(theX, theY);
+     }
+
+     getInfo(): string {
+         return super.getInfo() + ` radius=${this._radius}`;
+     }
+}
---
+++ typescript-training/05-inheritance/Driver.ts
@@ -0,0 +1,13 @@
+import { Shape } from './Shape';
+import { Circle } from './Circle';
+import { Rectangle } from './Rectangle';
+
+
+let myShape = new Shape(10,15);
+console.log(myShape);
+
+let myCircle = new Circle(5,10,15);
+console.log(myCircle);
+
+let myRectangle = new Rectangle(0,0,3,7);
+console.log(myRectangle);
