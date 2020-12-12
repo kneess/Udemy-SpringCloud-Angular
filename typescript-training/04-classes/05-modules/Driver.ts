a828faddaae2a992f1f4b92bfcb12f45975e5615
kneess <anibalcampos2324@gmail.com>
2020-12-11 18:02:39 -0700

    classes

---
+++ typescript-training/04-classes/05-modules/Driver.ts
@@ -0,0 +1,7 @@
+import { Customer } from './Customer';
+
+//let's create an instance of our class
+let myCustomer = new Customer("Martin", "Dixon");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
