d41a2838c59dd44d61915cc7efdc5f851b1d43e4
kneess <anibalcampos2324@gmail.com>
2020-12-16 19:06:48 -0700

    dynamic router links exposed ids

--- ecommerce-project/03-Frontend/angular-ecommerce/src/app/services/product.service.ts
+++ ecommerce-project/03-Frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,8 @@ export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products';
 
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
@@ -19,15 +22,27 @@ export class ProductService {
     // need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
   
-    console.log(searchUrl);
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
+
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
