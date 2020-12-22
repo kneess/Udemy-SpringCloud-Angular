82581a612c31cd4a8e3f95b94e3179ff1793f787
kneess <anibalcampos2324@gmail.com>
2020-12-22 08:29:30 -0700

    add custom validation

--- ecommerce-project/03-Frontend/angular-ecommerce2/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-Frontend/angular-ecommerce2/src/app/components/checkout/checkout.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
+import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
 
 @Component({
   selector: 'app-checkout',
@@ -33,8 +34,8 @@ export class CheckoutComponent implements OnInit {
 
     this.checkOutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: new FormControl('',[Validators.required, Validators.minLength(2)]),
-        lastName: new FormControl('',[Validators.required, Validators.minLength(2)]),
+        firstName: new FormControl('',[Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhiteSpace]),
+        lastName: new FormControl('',[Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhiteSpace]),
         email: new FormControl('',
                         [Validators.required, Validators.pattern('^a-z0-9._%+-@[a-z0-9.-]+\\.[a-z]{2,4}$')]
         )
