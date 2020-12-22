6b8fbca1472ad03c96c92b4ebc44af3e5c410237
kneess <anibalcampos2324@gmail.com>
2020-12-21 20:56:52 -0700

    billing&shipping form populating

--- ecommerce-project/02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/CountryRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/CountryRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
-@RepositoryRestResource(collectionResourceRel = "coutries", path = "countries")
+@RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 }
--- ecommerce-project/03-Frontend/angular-ecommerce2/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-Frontend/angular-ecommerce2/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,8 @@
+import { JsonpClientBackend } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 
 @Component({
@@ -16,6 +19,12 @@ export class CheckoutComponent implements OnInit {
 
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
+
+  countries: Country[] = [];
+  
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
 
   constructor(private formBuilder: FormBuilder,
               private luv2ShopFormService: Luv2ShopFormService) { }
@@ -70,21 +79,40 @@ export class CheckoutComponent implements OnInit {
         this.creditCardYears = data;
       }
     )
+
+    //populate countries
+    this.luv2ShopFormService.getCountries().subscribe(
+      data => {
+        console.log("Retrieved countries: "+ JSON.stringify(data));
+        this.countries = data;
+      }
+    );
+
+    //ngOnInit end bracket
   }
 
   onSubmit() {
     console.log("handling the submit button");
     console.log(this.checkOutFormGroup.get('customer').value);
     console.log("The email address is: " + this.checkOutFormGroup.get('customer').value.email);
+
+    console.log("The shipping address country is: " + this.checkOutFormGroup.get('shippingAddress').value.country.name);
+    console.log("The shipping address state is: " + this.checkOutFormGroup.get('shippingAddress').value.state.name);
   }
 
   copyShippingAddressToBillingAddress(event){
     if(event.target.checked){
       this.checkOutFormGroup.controls.billingAddress
         .setValue(this.checkOutFormGroup.controls.shippingAddress.value);
+
+        //bug fix for states
+        this.billingAddressStates = this.shippingAddressStates;
     }
     else {
       this.checkOutFormGroup.controls.billingAddress.reset();
+
+      //bug fix for states
+      this.billingAddressStates = [];
     }
   }
 
@@ -113,4 +141,31 @@ export class CheckoutComponent implements OnInit {
       }
     )
   }
+
+  getStates(formGroupName: string){
+
+    const formGroup = this.checkOutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.luv2ShopFormService.getStates(countryCode).subscribe(
+      data => {
+
+        if(formGroupName === 'shippingAddress'){
+          this.shippingAddressStates = data;
+        }
+        else {
+          this.billingAddressStates = data;
+        }
+
+        // select first item by default
+        formGroup.get('state').setValue(data[0]);
+      }
+    )
+  };
+
 }
